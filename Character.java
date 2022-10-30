f9280f955b74d4350b8633a2f78aba21f1b8c0d3
Sarfaraz Zulfiqar <115269960+Sarfaraz93@users.noreply.github.com>
2022-10-30 12:07:33 +0500

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,11 @@
+class character{
+      public static void main(String[]args){
+      System.out.println("I am Sarfaraz Zulfiqar\n19 years old "); //\n for newline
+      System.out.println("I am currently studying Computer Science\t1st year at the University of Sindh(IMCS)");  //\t for 10 spaces 
+      System.out.println("I want to become a computer scientist\tand achieve all my goals"); //\r for carriage retain
+      System.out.println("I am Sarfaraz Zulfiqar\n19 years old "); 
+      System.out.println("I have two goals in life \b first Army Officer  \b and second Computer Scientist"); //b for backspace
+      System.out.println("My favourite book is The Rich Dad Poor Dad \\\\ by Robert T Kiyosaki"); // \\single backslash
+      System.out.println("My favourite games are basketball and Cricket"); // \"for single quote
+    }
+}
